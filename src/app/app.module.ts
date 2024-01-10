ee41d488de9fadebb8500e283c4e316b19ca61c5
Damian2340842 <damianparedes842@gmail.com>
2024-01-10 01:45:38 -0500

    d
    aa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'roles', component: RolesComponent },
+  { path: 'Header', component: HeaderComponent },
+
   { path: 'recuperacion-contrasena', component: RecuperacionContrasenaComponent },
 ];
 
