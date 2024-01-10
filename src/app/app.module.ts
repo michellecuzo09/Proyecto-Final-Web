ca4b398e96d42cb2d95b9f993c112e40a1b38f32
BryanOrtega08 <bryan.ortega.est@tecazuay.edu.ec>
2024-01-10 01:03:38 -0500

    B

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+];
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
