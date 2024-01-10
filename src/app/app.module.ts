a037ce755446f27df49f8958fd1c036d4159a55f
¨Maikol <maikoltj11@hotmail.es>
2024-01-10 00:37:45 -0500

    Maikol
    Eliminacion de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
-import { RegistroComponent } from './registro/registro.component';
-import { VentanaPrincipalComponent } from './ventana-principal/ventana-principal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IniciarSesionComponent,
-    RegistroComponent,
-    VentanaPrincipalComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
