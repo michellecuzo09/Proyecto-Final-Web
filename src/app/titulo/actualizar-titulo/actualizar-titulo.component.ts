b2160980adce41543ca058b836f1d12803d58f9a
Damian2340842 <damianparedes842@gmail.com>
2024-01-18 23:31:29 -0500

    da
    titulo metodos

---
+++ src/app/titulo/actualizar-titulo/actualizar-titulo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-titulo',
+  templateUrl: './actualizar-titulo.component.html',
+  styleUrls: ['./actualizar-titulo.component.css']
+})
+export class ActualizarTituloComponent {
+
+}
